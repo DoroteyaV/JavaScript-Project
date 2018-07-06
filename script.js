ab1123183c163f0dc82c5bb8dc5064f440bcf9a6
Unknown <dmvladova@gmail.com>
2018-07-06 08:39:50 +0300

    onclick buttons work

--- script.js
+++ script.js
@@ -12,10 +12,31 @@ var arrIngredients = [  ['Крило от прилеп', 'A'],
 	arrLen = arrIngredients.length;
 var target = document.getElementById('wrapper'),
 	btnEl, i;
+
 for (i = 0; i < arrLen; i++){
 	btnEl = document.createElement('button');
 	btnEl.textContent = arrIngredients[i][0];
 	btnEl.setAttribute('id', i+1);
 	btnEl.setAttribute('value', arrIngredients[i][1]);
+	btnEl.setAttribute('class', 'magic');
 	target.appendChild(btnEl);
 }
+var arrMagics = [];
+
+$('.magic').on('click', function(e){
+    e.preventDefault();    
+    arrMagics.push( $(this).val() );
+   // console.log(arrMagics); // just so you can see the content
+    
+});
+
+$('#check').on('click', function() {
+    console.log(arrMagics.length); // < read the length of the amended array here
+    console.log(arrMagics); // just so you can see the content
+})
+// $('button').on('click', function(){	
+// 	var magic = $(this).value;			
+// 	arrMagics.push(magic);
+// 	//console.log(arrMagics);
+// });
+// console.log(arrMagics.push(magic));
